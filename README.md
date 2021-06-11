c248257ca34904a5fcdd1bccbd3dd97d71d3369b
Mechoshi Puhanaga <mechokio@gmail.com>
2021-06-12 00:40:01 +0300

    Add tests for horizontal scroller; debouncing scroller size calculation; no init timeout for scroller size calculation

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 | Statements                                                            | Branches                                                            | Functions                                                         | Lines                                                            |
 | --------------------------------------------------------------------- | ------------------------------------------------------------------- | ----------------------------------------------------------------- | ---------------------------------------------------------------- |
-| ![Statements](https://img.shields.io/badge/statements-74.73%25-red.svg) | ![Branches](https://img.shields.io/badge/branches-49.17%25-red.svg) | ![Functions](https://img.shields.io/badge/functions-70%25-red.svg) | ![Lines](https://img.shields.io/badge/lines-75.14%25-red.svg) |
+| ![Statements](https://img.shields.io/badge/statements-79.03%25-red.svg) | ![Branches](https://img.shields.io/badge/branches-55%25-red.svg) | ![Functions](https://img.shields.io/badge/functions-76.67%25-red.svg) | ![Lines](https://img.shields.io/badge/lines-78.92%25-red.svg) |
 
 ## React version
 
--- src/Scroll/tests/Scroll.test.js
+++ src/Scroll/tests/Scroll.test.js
@@ -7,34 +7,59 @@ import { Scroll } from '../index';
 import './ScrollTest.css';
 
 const containerClass = 'react-scroll-component';
-const contentClass = 'content';
-const scrollerClass = 'scroller';
+
+const contentClassVertical = 'content-vertical';
+const scrollerClassVertical = 'scroller-vertical';
 
 const basicContainerHeight = 150;
 const basicContentHeight = 1000;
+
+const contentClassHorizontal = 'content-horizontal';
+const scrollerClassHorizontal = 'scroller-horizontal';
+
+const basicContainerWidth = 150;
+const basicContentWidth = 1000;
 
 // TODO: Math.floor exposes implementation detail. Fix this!
 const basicExpectedScrollerHeight = Math.floor(
   (basicContainerHeight / basicContentHeight) * basicContainerHeight
 );
 
-const BasicVertical = () => {
-  return (
-    <Scroll
-      direction="vertical"
-      height={`${basicContainerHeight}px`}
-      scrollerClass={scrollerClass}
-    >
-      <div className={contentClass}>Hi</div>
-    </Scroll>
-  );
-};
+const basicExpectedScrollerWidth = Math.floor(
+  (basicContainerWidth / basicContentWidth) * basicContainerWidth
+);
+
+const BasicVertical = (props = {}) => (
+  <Scroll
+    direction="vertical"
+    height={`${basicContainerHeight}px`}
+    scrollerClass={scrollerClassVertical}
+    {...props}
+  >
+    {props.children}
+  </Scroll>
+);
+
+const BasicHorizontal = (props = {}) => (
+  <Scroll
+    direction="horizontal"
+    width={`${basicContainerWidth}px`}
+    scrollerClass={scrollerClassHorizontal}
+    {...props}
+  >
+    {props.children}
+  </Scroll>
+);
 
 describe('Scroll', () => {
-  it('Scroll content when moving the scroller', () => {
-    mount(<BasicVertical />);
+  it('Scroll content vertically when moving the scroller', () => {
+    mount(
+      <BasicVertical>
+        <div className={contentClassVertical}>Hi</div>
+      </BasicVertical>
+    );
 
-    const scroller = cy.get(`.${scrollerClass}`);
+    const scroller = cy.get(`.${scrollerClassVertical}`);
     scroller.trigger('mousedown');
     scroller.trigger('touchmove', { changedTouches: [{ pageY: 100 }] });
     scroller.trigger('touchend');
@@ -43,13 +68,17 @@ describe('Scroll', () => {
     container.invoke('scrollTop').should('be.gt', 0); // TODO: be precise here
   });
 
-  it('Move scroller when scrolling over the content', () => {
-    mount(<BasicVertical />);
+  it('Move scroller vertically when scrolling over the content', () => {
+    mount(
+      <BasicVertical>
+        <div className={contentClassVertical}>Hi</div>
+      </BasicVertical>
+    );
 
     const container = cy.get(`.${containerClass}`);
     container.scrollTo('bottom');
 
-    const scroller = cy.get(`.${scrollerClass}`);
+    const scroller = cy.get(`.${scrollerClassVertical}`);
     scroller
       .should('have.attr', 'style')
       .should(
@@ -58,5 +87,77 @@ describe('Scroll', () => {
           basicContainerHeight - basicExpectedScrollerHeight
         }px, 0px)`
       );
+  });
+
+  it('Scroll content horizontally when moving the scroller', () => {
+    mount(
+      <BasicHorizontal>
+        <div className={contentClassHorizontal}>Hi</div>
+      </BasicHorizontal>
+    );
+
+    const scroller = cy.get(`.${scrollerClassHorizontal}`);
+    scroller.trigger('mousedown');
+    scroller.trigger('touchmove', { changedTouches: [{ pageX: 100 }] });
+    scroller.trigger('touchend');
+
+    const container = cy.get(`.${containerClass}`);
+    container.invoke('scrollLeft').should('be.gt', 0); // TODO: be precise here
+  });
+
+  it('Move scroller horizontally when scrolling over the content', () => {
+    mount(
+      <BasicHorizontal>
+        <div className={contentClassHorizontal}>Hi</div>
+      </BasicHorizontal>
+    );
+
+    const container = cy.get(`.${containerClass}`);
+    container.scrollTo('right');
+
+    const scroller = cy.get(`.${scrollerClassHorizontal}`);
+    scroller
+      .should('have.attr', 'style')
+      .should(
+        'contain',
+        `translate3d(${
+          basicContainerWidth - basicExpectedScrollerWidth
+        }px, 0px, 0px)`
+      );
+  });
+
+  it('Debounce scroller size calculation', () => {
+    mount(
+      <BasicVertical scrollSizeDebounce={2000}>
+        <div className={contentClassVertical}>Hi</div>
+      </BasicVertical>
+    );
+
+    const container = cy.get(`.${containerClass}`);
+    container.scrollTo('bottom');
+
+    const scroller = cy.get(`.${scrollerClassVertical}`);
+    scroller.invoke('height').should('gt', 0);
+  });
+
+  it('No initial timeout for calculating scroller size', () => {
+    mount(
+      <BasicVertical noInitTimeout>
+        <div className={contentClassVertical}>Hi</div>{' '}
+      </BasicVertical>
+    );
+
+    const container = cy.get(`.${containerClass}`);
+    container.scrollTo('bottom');
+
+    const scroller = cy.get(`.${scrollerClassVertical}`);
+    scroller.invoke('height').should('gt', 0);
+  });
+
+  it('Mount without children', () => {
+    mount(<BasicVertical />);
+
+    const container = cy.get(`.${containerClass}`);
+    container.should('not.exist');
   });
 });
